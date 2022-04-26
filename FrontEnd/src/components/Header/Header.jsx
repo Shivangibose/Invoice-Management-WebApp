dcd66fcfbaabd398c1d2f4a2d50a07211b77fb79
Shivangi Bose <53455642+Shivangibose@users.noreply.github.com>
2022-04-26 20:48:13 +0530

    Update Header.jsx

--- FrontEnd/src/components/Header/Header.jsx
+++ FrontEnd/src/components/Header/Header.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import "./Header.css";
 import ABCLogo from "../Images/ABCLogoFull.svg";
-import hrcLogo from "../Images/hrcLogo.svg";
+
 
 function Header() {
   return (
     <div className="head">
     <div className="head1">
     <img className="ABC" src={ABCLogo}></img>
-    </div>
-    
-    <div className="head2">
-    <img className="hrclogo" src={hrcLogo}></img>
-    </div>
-    
+    </div>   
 
     </div>
   )
